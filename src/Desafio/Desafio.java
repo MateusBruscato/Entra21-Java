7200ea46229cfc4175349558b06f6fd807c2dabb
Mateus Bruscato <mateus.bruscato@gmail.com>
2022-05-18 21:20:29 -0300

    Primeira versão do Sistema de Gestão de Hotel

--- src/Desafio/Desafio.java
+++ src/Desafio/Desafio.java
@@ -12,8 +12,6 @@ import java.util.Scanner;
  */
 public class Desafio {
 
-	private static final String ArrayI = null;
-
 	static Scanner entrada = new Scanner(System.in);
 
 	static ArrayList<ArrayList<String>> matrix = new ArrayList<ArrayList<String>>();
@@ -21,6 +19,8 @@ public class Desafio {
 	static ArrayList<ArrayList<String>> quartos = new ArrayList<ArrayList<String>>();
 
 	static ArrayList<ArrayList<String>> check = new ArrayList<ArrayList<String>>();
+
+	static ArrayList<String> freeRooms = new ArrayList<String>();
 
 	static String menu = "Sistema de Gestão de Hotel \n\n" + "1 - Cadastrar Cliente \n"
 			+ "2 - Editar ou exlcuir cliente \n" + "3 - Cadastrar Quarto \n" + "4 - Editar Cadastro de Quarto \n"
@@ -32,10 +32,20 @@ public class Desafio {
 	 */
 	public static void main(String[] args) {
 		int choice;
-		int costmNumb = 0;
+		int costmNumb = 1;
 		int roomNumb = 0;
 		int checkCount = 0;
 
+		matrix.add(new ArrayList<String>());
+		matrix.get(0).add("Nome: ");
+		matrix.get(0).add("Data de nascimento: ");
+		matrix.get(0).add("E-mail: ");
+		matrix.get(0).add("Telefone: ");
+		matrix.get(0).add("Cidade: ");
+		matrix.get(0).add("UF: ");
+		matrix.get(0).add("Fumante: ");
+		
+		
 		do {
 			System.out.println(menu);
 			choice = Integer.valueOf(entrada.next());
@@ -46,9 +56,8 @@ public class Desafio {
 			case 1:
 
 				entrada.useDelimiter(System.lineSeparator());
-
-				System.out.println("Nome: ");
 				matrix.add(new ArrayList<String>());
+				System.out.println("Nome: ");
 				matrix.get(costmNumb).add(entrada.next());
 
 				System.out.println("Data de nascimento: ");
@@ -74,6 +83,12 @@ public class Desafio {
 					matrix.get(costmNumb).set(6, entrada.next());
 				}
 
+				System.out.println("Cadastro do cliente " + matrix.get(costmNumb).get(0) + ": \n");
+				System.out.println("Código do cliente: " + costmNumb);
+				for (int i = 1; i < matrix.get(costmNumb).size(); i++)
+				{
+					System.out.println(matrix.get(0).get(i) + matrix.get(costmNumb).get(i));
+				}
 				costmNumb++;
 				break;
 
@@ -108,10 +123,11 @@ public class Desafio {
 									break;
 								}
 							} else if (edit == 2) {
-								System.out.println("Qual informação você gostaria de editar?" + "1 - Nome \n"
+								System.out.println("Qual informação você gostaria de editar?\n" + "1 - Nome \n"
 										+ "2 - Data de nascimento\n" + "3 - E-mail \n" + "4 - Telefone \n"
 										+ "5 - Cidade \n" + "6 - UF" + "7 - Fumante");
 								int ans2 = Integer.valueOf(entrada.next());
+								System.out.println("Digite a nova informação: ");
 								matrix.get(costmCode).set((ans2 - 1), entrada.next());
 
 							} else {
@@ -131,7 +147,7 @@ public class Desafio {
 
 			case 3:
 
-				System.out.println("Nome: ");
+				System.out.println("Número do quarto: ");
 				quartos.add(new ArrayList<String>());
 				quartos.get(roomNumb).add(entrada.next());
 
@@ -154,11 +170,13 @@ public class Desafio {
 
 				System.out.println("Disponibilidade de pessoas por quarto:  ");
 				quartos.get(roomNumb).add(entrada.next());
-				
+
+				quartos.get(roomNumb).add("f");
+
 				roomNumb++;
 				break;
 			case 4:
-				
+
 				try {
 
 					int ans;
@@ -209,47 +227,98 @@ public class Desafio {
 				}
 				break;
 
-			case 5: 
-				
+			case 5:
+
 				for (int i = 0; i < quartos.size(); i++) {
-					System.out.println(quartos.get(i).get(0));
+					if (quartos.get(i).get(6).equals("f")) {
+						System.out.println(quartos.get(i).get(0));
+					} else {
+						continue;
+					}
 				}
-				
 				break;
+
 			case 6:
-				
-				
+
 				System.out.println("Qual o código do cliente que gostaria de fazer Check-In?");
 				int costumer = Integer.valueOf(entrada.next());
-			
-				System.out.println("Cliente: " + matrix.get(costumer).get(0) + ". Deseja continuar? \n" + " 1 - Sim \n" + "2 - Não");
+
+				System.out.println("Cliente: " + matrix.get(costumer).get(0) + ". Deseja continuar? \n" + " 1 - Sim \n"
+						+ "2 - Não");
 				int ans = Integer.valueOf(entrada.next());
-				
+
 				if (ans == 1) {
 					check.add(new ArrayList<String>());
-
+					// Adicionar primeira linha na matriz
 					check.get(checkCount).add(Integer.toString(costumer));
-					
-					System.out.println("Qual quarto o cliente " + matrix.get(costumer).get(0) + "deseja fazer check in?" );
+					check.get(checkCount).add(Integer.toString(-1));
+
+					System.out.println(
+							"Qual quarto o cliente " + matrix.get(costumer).get(0) + " deseja fazer check in?");
 					int room = Integer.valueOf(entrada.next());
-					
+
 					for (int i = 0; i < quartos.size(); i++) {
-						if ( room == Integer.valueOf(quartos.get(i).get(0))) {
+						if (quartos.get(i).get(6).equals("t") && quartos.get(i).get(0).equals(Integer.toString(room))) {
 							System.out.println("Este quarto já está ocupado!");
-						} else {
-							check.get(checkCount).add(Integer.toString(room));
+						} else if (quartos.get(i).get(0).equals(Integer.toString(room))) {
+							check.get(checkCount).set(1, Integer.toString(room));
+							quartos.get(i).set(6, "t");
 							checkCount++;
+							System.out.println("Check-In realizado com sucesso!");
 						}
 					}
-					
-					
+				} else {
+					break;
 				}
+
+				break;
 			case 7:
 
+				System.out.println("Qual o código do cliente que gostaria de fazer Check-Out?");
+				costumer = Integer.valueOf(entrada.next());
+
+				System.out.println("Cliente: " + matrix.get(costumer).get(0) + ". Deseja continuar? \n" + " 1 - Sim \n"
+						+ "2 - Não");
+				ans = Integer.valueOf(entrada.next());
+
+				if (ans == 1) {
+					for (int i = 0; i < check.size(); i++) {
+						if (check.get(i).get(0).equals(Integer.toString(costumer))) {
+							for (int j = 0; j < quartos.size(); j++) {
+								if (quartos.get(j).get(0).equals((check.get(i).get(1)))) {
+									quartos.get(j).set(6, "f");
+									checkCount--;
+									check.remove(i);
+									System.out.println("Check-Out realizado com sucesso!");
+								}
+							}
+						}
+					}
+				}
+				break;
+				
+			case 0:
+				System.out.println("Tem certeza que deseja sair?\n" + "1 - Sim \n" + "2 - Não \n");
+				System.out.println("Resposta: ");
+				int sure = Integer.valueOf(entrada.next());
+
+				if (sure == 1) {
+					System.out.println("Programa finalizado!");
+					return;
+				} else if (sure == 2) {
+					break;
+				} else {
+					System.out.println("Opção inválida!");
+					break;
+				}
+
+			default:
+				System.out.println("Opção Inválida! \n");
+				break;
 			}
 			System.out.println(matrix);
 
-		} while (choice < 8 && choice >= 0);
+		} while (true);
 
 	}
 
